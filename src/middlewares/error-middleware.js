e3d0185b17a3860b0fdd963613496b364b464192
Triddov <akinin.stepan7@gmail.com>
2025-06-22 15:35:18 +0300

    feat(error-middleware.js)

---
+++ src/middlewares/error-middleware.js
@@ -0,0 +1,16 @@
+import ApiError from "../exceptions/api-error.js";
+
+
+const errorMiddleware = (err, req, res, next) => {
+    console.log(err);
+
+    if (err instanceof ApiError) {
+        return res.status(err.status).json({
+            message: err.message,
+            error: err.errors,
+        });
+    }
+    return res.status(500).json({message: `Unexpected error: ${err.message}`});
+}
+
+export default errorMiddleware();
